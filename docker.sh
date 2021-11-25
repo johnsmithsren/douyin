55037c1f584c56b2306d22be66979b1862f45905
johnsmithsren <rjm1149104294@outlook.com>
2021-11-25 17:53:04 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,3 @@
 echo "run douyin docker"
 
 docker build -t joyorigin-platform-douyin -f ./Dockerfile .
-docker login office.joyorigin.com:11080/
-docker tag joyorigin-platform-douyin:latest office.joyorigin.com:11080/ops/joyorigin-platform-douyin:latest
-docker push office.joyorigin.com:11080/ops/joyorigin-platform-douyin:latest
